2eba97fc641782534f4fcd83914bdbf9b375fab5
Dave Roberts <dave@davetheitguy.co.uk>
2023-08-21 00:17:51 +0100

    Created Project

---
+++ src/main/kotlin/uk/co/davetheitguy/tiqqet/TiqqetApplication.kt
@@ -0,0 +1,11 @@
+package uk.co.davetheitguy.tiqqet
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TiqqetApplication
+
+fun main(args: Array<String>) {
+    runApplication<TiqqetApplication>(*args)
+}
---
+++ src/test/kotlin/uk/co/davetheitguy/tiqqet/TiqqetApplicationTests.kt
@@ -0,0 +1,13 @@
+package uk.co.davetheitguy.tiqqet
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TiqqetApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
